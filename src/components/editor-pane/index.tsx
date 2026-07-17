4f1ae7d1531023f45f371773199b08ddcab05e62
issam eljaouhary <iseljao89@gmail.com>
2026-07-17 13:41:27 +0100

    feat: update EditorPane to utilize useAppStore for dynamic PrimitiveEditor key

--- src/components/editor-pane/index.tsx
+++ src/components/editor-pane/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import { PrimitiveEditor } from "./primitive-editor";
+import { useAppStore } from "@/store";
 
 interface EditorPaneProps extends Omit<
   React.HTMLAttributes<HTMLElement>,
@@ -8,12 +9,23 @@ interface EditorPaneProps extends Omit<
 > {}
 
 export function EditorPane({ className, ...props }: EditorPaneProps) {
+  const primitiveId = useAppStore((state) => {
+    const primitiveEditorView = state.primitiveEditorView;
+    if (!primitiveEditorView) {
+      return null;
+    }
+    const { mode, data } = primitiveEditorView;
+    if (mode === "edit") {
+      return data.id;
+    }
+    return null;
+  });
   return (
     <div
       className={twMerge("bg-card h-full flex flex-col p-2", className)}
       {...props}
     >
-      <PrimitiveEditor />
+      <PrimitiveEditor key={primitiveId} />
     </div>
   );
 }
